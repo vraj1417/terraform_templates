c398273954a885726df99a16fe41dd43d8ce9a2e
vraj1417 <139256074+vraj1417@users.noreply.github.com>
2024-03-22 13:51:54 -0500

    Update simple_architecture.tf

--- simple_architecture.tf
+++ simple_architecture.tf
@@ -1,5 +1,7 @@
 provider "aws" {
-  region = "us-east-1" # or your preferred region
+  region     = "us-east-1"
+  access_key = "your_access_key_here"   # It is recommended to use envirionmental variables here
+  secret_key = "your_secret_key_here"   # instead of putting the information in plaintext
 }
 
 
