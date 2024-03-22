75aec0cfd4dcd3cdead2c8937d69f00f33b0d872
vraj1417 <139256074+vraj1417@users.noreply.github.com>
2024-03-22 13:54:22 -0500

    Update simple_architecture.tf

--- simple_architecture.tf
+++ simple_architecture.tf
@@ -100,7 +100,7 @@ resource "aws_security_group" "my_sg" {
   ingress {
     from_port   = 80
     to_port     = 80
-    protocol    = "tcp"
+    protocol    = "HTTP"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
